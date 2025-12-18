66550e7017322dfda741d77f4e94e52e1adcbf63
Cássio Rosso <61170321+cassiorosso@users.noreply.github.com>
2025-12-17 22:44:48 -0300

    Update API

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 
 /* -------------------------------------------------------------------------- */
 
-app.get('/', (req, res) => res.send("Stripe Webhook API is up!"));
+app.get('/', (req, res) => res.send("Stripe Webhook API is up - 2025-11-17.clover"));
 
 /* -------------------------------------------------------------------------- */
 /* Webhook                                                                     */
